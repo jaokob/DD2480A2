de7dfb3273dbaeb7ae64b641326e23465fce8c28
Jenny Norelius <jenny.norelius@gmail.com>
2019-02-04 09:26:26 +0100

    The compiler now takes a path as an argument. Implemented tests for build. As per issue #4.

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-javac commits/$1/src/*.java
-jar -cvf commits/$1/comp.jar commits/$1/src/*.class
-rm commits/$1/src/*.class
-
-#-d commits/$1
+javac $1/src/*.java
+jar -cvf $1/comp.jar $1/src/*.class
+rm $1/src/*.class
