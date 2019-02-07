9ae78e10c9798e9bd191e2d098ef6ac38b901dba
fredde <frlof@kth.se>
2019-02-07 15:22:46 +0100

    fixing problems mentioned in review in pull request #23. issue #18

--- testing.sh
+++ testing.sh
@@ -3,6 +3,5 @@
 if [ ! -d $1/out ]; then
 	mkdir $1/out
 fi
-javac -d $1/out $1/src/Decide.java
 javac -d $1/out -cp "lib/junit-platform-console-standalone-1.3.2.jar" $1/test/*.java $1/src/*.java
 java -jar "lib/junit-platform-console-standalone-1.3.2.jar" --class-path $1/out --scan-class-path
